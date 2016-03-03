f5ae844c7f4a7cb50d218f3453ba77a876008179
Ben Kuper <bkuperberg@gmail.com>
2016-03-03 11:27:16 +0100

    Added lookAndFeelOO and audioMixerNode

---
+++ src/Blooper/Source/AudioMixerNode.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    AudioMixerNode.h
+    Created: 3 Mar 2016 10:14:46am
+    Author:  bkupe
+
+  ==============================================================================
+*/
+
+#ifndef AUDIOMIXERNODE_H_INCLUDED
+#define AUDIOMIXERNODE_H_INCLUDED
+
+
+
+
+
+#endif  // AUDIOMIXERNODE_H_INCLUDED
---
+++ src/Blooper/Source/LookAndFeelOO.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    LookAndFeelOO.h
+    Created: 3 Mar 2016 10:21:45am
+    Author:  bkupe
+
+  ==============================================================================
+*/
+
+#ifndef LOOKANDFEELOO_H_INCLUDED
+#define LOOKANDFEELOO_H_INCLUDED
+
+
+
+
+
+#endif  // LOOKANDFEELOO_H_INCLUDED
