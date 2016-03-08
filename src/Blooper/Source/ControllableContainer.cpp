dc2d4ec87aa081855be44fd59b0a17013b6a1f67
Ben Kuper <bkuperberg@gmail.com>
2016-03-08 14:10:30 +0100

    controllableContainer update

--- src/Blooper/Source/ControllableContainer.cpp
+++ src/Blooper/Source/ControllableContainer.cpp
@@ -15,8 +15,70 @@ ControllableContainer::ControllableContainer(const String & shortName) : shortNa
 
 }
 
+ControllableContainer::~ControllableContainer()
+{
+	controllables.clear();
+}
+
+FloatParameter * ControllableContainer::addFloatParameter(const String & shortName, const float & initialValue, const float & minValue, const float & maxValue, const bool & enabled)
+{
+	if (getControllableByName(shortName) != nullptr)
+	{
+		DBG("ControllableContainer::add parameter, short Name already exists : " + shortName);
+		return;
+	}
+
+	FloatParameter * p = new FloatParameter(shortName, initialValue, minValue, maxValue, enabled);
+	controllables.add(p);
+	listeners.call(&Listener::controllableAdded, p);
+	return p;
+}
+
+IntParameter * ControllableContainer::addIntParameter(const String & shortName, const int & initialValue, const int & minValue, const int & maxValue, const bool & enabled)
+{
+	if (getControllableByName(shortName) != nullptr)
+	{
+		DBG("ControllableContainer::add parameter, short Name already exists : " + shortName);
+		return;
+	}
+
+	IntParameter * p = new IntParameter(shortName, initialValue, minValue, maxValue, enabled);
+	controllables.add(p);
+	listeners.call(&Listener::controllableAdded, p);
+	return p;
+}
+
+BoolParameter * ControllableContainer::addBoolParameter(const String & shortName, const bool & value, const bool & enabled)
+{
+	if (getControllableByName(shortName) != nullptr)
+	{
+		DBG("ControllableContainer::add parameter, short Name already exists : " + shortName);
+		return;
+	}
+
+	BoolParameter * p = new BoolParameter(shortName, value, enabled);
+	controllables.add(p);
+	listeners.call(&Listener::controllableAdded, p);
+	return p;
+}
+
+Trigger * ControllableContainer::addTrigger(const String & shortName, const bool & enabled)
+{
+	if (getControllableByName(shortName) != nullptr)
+	{
+		DBG("ControllableContainer::add trigger, short Name already exists : " + shortName);
+		return;
+	}
+
+	Trigger * t = new Trigger(shortName, enabled);
+	controllables.add(t);
+	listeners.call(&Listener::controllableAdded, t);
+	return t;
+}
+
 void ControllableContainer::removeControllable(Controllable * c)
 {
+	listeners.call(&Listener::controllableRemoved, c);
 	controllables.removeObject(c);
 }
 
--- src/Blooper/Source/ControllableContainer.h
+++ src/Blooper/Source/ControllableContainer.h
@@ -12,6 +12,10 @@
 #define CONTROLLABLECONTAINER_H_INCLUDED
 
 #include "Controllable.h"
+#include "FloatParameter.h"
+#include "IntParameter.h"
+#include "BoolParameter.h"
+#include "Trigger.h"
 
 class ControllableContainer
 {
@@ -22,10 +26,10 @@ public:
 	String shortName;
 	OwnedArray<Controllable> controllables;
 
-	Controllable * addFloatParameter(const String &shortName, const float &initialValue, const float &minValue = 0, const float &maxValue = 1, const bool &enabled = true);
-	Controllable * addIntParameter(const String &shortName, const int &initialValue, const int &minValue, const int &maxValue, const bool &enabled = true);
-	Controllable * addBoolParameter(const String &shortName, const bool &value, const bool &enabled = true);
-	Controllable * addTriggerParameter(const String &shortName, const bool &enabled = true);
+	FloatParameter * addFloatParameter(const String &shortName, const float &initialValue, const float &minValue = 0, const float &maxValue = 1, const bool &enabled = true);
+	IntParameter * addIntParameter(const String &shortName, const int &initialValue, const int &minValue, const int &maxValue, const bool &enabled = true);
+	BoolParameter * addBoolParameter(const String &shortName, const bool &value, const bool &enabled = true);
+	Trigger * addTrigger(const String &shortName, const bool &enabled = true);
 
 	void removeControllable(Controllable * c);
 	Controllable * getControllableByName(const String &name);
@@ -38,8 +42,8 @@ public:
 	public:
 		/** Destructor. */
 		virtual ~Listener() {}
-		virtual void controllerAdded(Controllable * c) = 0;
-		virtual void controllerRemoved(Controllable * c) = 0;
+		virtual void controllableAdded(Controllable * c) = 0;
+		virtual void controllableRemoved(Controllable * c) = 0;
 	};
 
 	ListenerList<Listener> listeners;
