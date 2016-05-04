b444917c8b40f0fd6249bcffbef0ce0ee68b0878
Ben Kuper <bkuperberg@gmail.com>
2016-05-04 17:11:39 +0200

    started RuleManager (continue in feature)

---
+++ src/LGML/Source/ControlRuleManagerUI.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    ControlRuleManagerUI.h
+    Created: 4 May 2016 5:07:07pm
+    Author:  bkupe
+
+  ==============================================================================
+*/
+
+#ifndef CONTROLRULEMANAGERUI_H_INCLUDED
+#define CONTROLRULEMANAGERUI_H_INCLUDED
+
+
+
+
+
+#endif  // CONTROLRULEMANAGERUI_H_INCLUDED
---
+++ src/LGML/Source/ScriptedRule.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    ScriptedRule.h
+    Created: 4 May 2016 5:08:15pm
+    Author:  bkupe
+
+  ==============================================================================
+*/
+
+#ifndef SCRIPTEDRULE_H_INCLUDED
+#define SCRIPTEDRULE_H_INCLUDED
+
+
+
+
+
+#endif  // SCRIPTEDRULE_H_INCLUDED
