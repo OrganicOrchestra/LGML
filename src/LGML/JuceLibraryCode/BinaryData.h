c6d33896cd03465f9074bf5074cfeab6dbce4480
Ben Kuper <bkuperberg@gmail.com>
2016-05-22 13:56:19 +0200

    repush win icon

--- src/LGML/JuceLibraryCode/BinaryData.h
+++ src/LGML/JuceLibraryCode/BinaryData.h
@@ -9,6 +9,9 @@
 
 namespace BinaryData
 {
+    extern const char*   littleredridinghoodmd_png;
+    const int            littleredridinghoodmd_pngSize = 37657;
+
     extern const char*   default_lgmllayout;
     const int            default_lgmllayoutSize = 2422;
 
@@ -28,7 +31,7 @@ namespace BinaryData
     extern const char* namedResourceList[];
 
     // Number of elements in the namedResourceList array.
-    const int namedResourceListSize = 5;
+    const int namedResourceListSize = 6;
 
     // If you provide the name of one of the binary resource variables above, this function will
     // return the corresponding data and its size (or a null pointer if the name isn't found).
