42a28534731ebe2b70d4be1c2db09ddf87983b4b
Martin Hermant <martin.hermant@gmail.com>
2016-06-21 15:26:09 +0200

    add pitchWheel support on midi controllers

--- src/LGML/Source/MIDIController.cpp
+++ src/LGML/Source/MIDIController.cpp
@@ -17,121 +17,133 @@
 MIDIController::MIDIController() :
 Controller("MIDI"),JsEnvironment("MIDI.MIDIController")
 {
-    setNamespaceName("MIDI."+nameParam->stringValue());
-    deviceInName = addStringParameter("midiPortName", "name of Midi device input", "");
-    scriptPath = addStringParameter("jsScriptPath", "path for js script", "");
-    logIncoming = addBoolParameter("logIncoming","log Incoming midi message",false);
+  setNamespaceName("MIDI."+nameParam->stringValue());
+  deviceInName = addStringParameter("midiPortName", "name of Midi device input", "");
+  scriptPath = addStringParameter("jsScriptPath", "path for js script", "");
+  logIncoming = addBoolParameter("logIncoming","log Incoming midi message",false);
 
-	for (int i = 0; i < 127; i++)
-	{
-		String noteName = MidiMessage::getMidiNoteName(i, true, true, 0);
-		FloatParameter * fp = new FloatParameter(noteName, "Value for " + noteName, 0, 0, 1);
-		addVariable(fp);
-	}
+  for (int i = 0; i < 127; i++)
+  {
+    String noteName = MidiMessage::getMidiNoteName(i, true, true, 0);
+    FloatParameter * fp = new FloatParameter(noteName, "Value for " + noteName, 0, 0, 1);
+    addVariable(fp);
+  }
 
-	for (int i = 0; i < 127; i++)
-	{
-		FloatParameter *fp = new FloatParameter(String("CC ") + String(i), String("ControlChange ") + String(i), 0, 0, 1);
-		addVariable(fp);
-	}
+  for (int i = 0; i < 127; i++)
+  {
+    FloatParameter *fp = new FloatParameter(String("CC ") + String(i), String("ControlChange ") + String(i), 0, 0, 1);
+    addVariable(fp);
+  }
 }
 
 MIDIController::~MIDIController()
 {
-	setCurrentDevice(String::empty);
+  setCurrentDevice(String::empty);
 }
 
 ControllerUI * MIDIController::createUI()
 {
-    return new MIDIControllerUI(this);
+  return new MIDIControllerUI(this);
 }
 
 void MIDIController::handleIncomingMidiMessage (MidiInput* ,
                                                 const MidiMessage& message)
 {
 
-    if (!enabledParam->boolValue()) return;
+  if (!enabledParam->boolValue()) return;
 
-    if(message.isController()){
-		if (logIncoming->boolValue())
-		{
-			NLOG("MIDI","CC "+String(message.getControllerNumber()) + " > " + String(message.getControllerValue()));
-		}
-
-		int variableIndex = 128 + message.getControllerNumber() -1;
-		DBG("Variable name " << variables[variableIndex]->parameter->niceName);
-		variables[variableIndex]->parameter->setValue(message.getControllerValue()*1.f / 127.f);
-    }
-    else if(message.isNoteOnOrOff()){
-		if (logIncoming->boolValue())
-		{
-			NLOG("MIDI", "Note " + String(message.isNoteOn() ? "on" : "off") + " : " + String(message.getNoteNumber()) + " > " + String(message.getVelocity()));
-		}
-
-		int variableIndex = message.getNoteNumber();
-		variables[variableIndex]->parameter->setValue(message.getVelocity()*1.f / 127.f);
+  if(message.isController()){
+    if (logIncoming->boolValue())
+    {
+      NLOG("MIDI","CC "+String(message.getControllerNumber()) + " > " + String(message.getControllerValue()));
     }
 
-    callJs(message);
-    activityTrigger->trigger();
+    int variableIndex = 128 + message.getControllerNumber() -1;
+    DBG("Variable name " << variables[variableIndex]->parameter->niceName);
+    variables[variableIndex]->parameter->setValue(message.getControllerValue()*1.f / 127.f);
+  }
+  else if(message.isNoteOnOrOff()){
+    if (logIncoming->boolValue())
+    {
+      NLOG("MIDI", "Note " + String(message.isNoteOn() ? "on" : "off") + " : " + String(message.getNoteNumber()) + " > " + String(message.getVelocity()));
+    }
+    int variableIndex = message.getNoteNumber();
+    variables[variableIndex]->parameter->setValue(message.getVelocity()*1.f / 127.f);
+  }
+
+  else if (message.isPitchWheel()){
+    if (logIncoming->boolValue())
+    {
+      NLOG("MIDI", "pitch wheel " + String(message.getPitchWheelValue()));
+    }
+  }
+
+  callJs(message);
+  activityTrigger->trigger();
 }
 
 void MIDIController::callJs(const MidiMessage& message){
-    if(message.isController()){
-        static const Identifier onCCFunctionName("onCC");
-        Array<var> args;
-        args.add(message.getControllerNumber());
-        args.add(message.getControllerValue());
-        callFunctionFromIdentifier(onCCFunctionName, args);
-    }
-    if(message.isNoteOnOrOff()){
-        static const Identifier onCCFunctionName("onNote");
-        Array<var> args;
-        args.add(message.getNoteNumber());
-        args.add(message.isNoteOn()?message.getVelocity():0);
-        callFunctionFromIdentifier(onCCFunctionName, args);
-    }
+  if(message.isController()){
+    static const Identifier onCCFunctionName("onCC");
+    Array<var> args;
+    args.add(message.getControllerNumber());
+    args.add(message.getControllerValue());
+    callFunctionFromIdentifier(onCCFunctionName, args);
+  }
+  else if(message.isNoteOnOrOff()){
+    static const Identifier onCCFunctionName("onNote");
+    Array<var> args;
+    args.add(message.getNoteNumber());
+    args.add(message.isNoteOn()?message.getVelocity():0);
+    callFunctionFromIdentifier(onCCFunctionName, args);
+  }
+  else if(message.isPitchWheel()){
+    static const Identifier onPitchWheelFunctionName("onPitchWheel");
+    Array<var> args;
+    args.add(message.getPitchWheelValue());
+    callFunctionFromIdentifier(onPitchWheelFunctionName, args);
+  }
 }
 
 void MIDIController::onContainerParameterChanged(Parameter * p){
-    if(p==nameParam){
-        setNamespaceName("MIDI."+nameParam->stringValue());
-    }
-    else if (p==deviceInName){
-        setCurrentDevice(deviceInName->stringValue());
-    }
-    else if(p==scriptPath){
-        loadFile(scriptPath->stringValue());
-    }
+  if(p==nameParam){
+    setNamespaceName("MIDI."+nameParam->stringValue());
+  }
+  else if (p==deviceInName){
+    setCurrentDevice(deviceInName->stringValue());
+  }
+  else if(p==scriptPath){
+    loadFile(scriptPath->stringValue());
+  }
 }
 
 void MIDIController::buildLocalEnv(){
-    DynamicObject obj;
-    static const Identifier jsSendCCIdentifier("sendCC");
-    obj.setMethod(jsSendCCIdentifier, sendCC);
-    obj.setProperty(jsPtrIdentifier, (int64)this);
+  DynamicObject obj;
+  static const Identifier jsSendCCIdentifier("sendCC");
+  obj.setMethod(jsSendCCIdentifier, sendCC);
+  obj.setProperty(jsPtrIdentifier, (int64)this);
 
-    static const Identifier jsSendNoteOnForIdentifier("sendNoteOnFor");
-    obj.setMethod(jsSendNoteOnForIdentifier, sendNoteOnFor);
-    setLocalNamespace(obj);
+  static const Identifier jsSendNoteOnForIdentifier("sendNoteOnFor");
+  obj.setMethod(jsSendNoteOnForIdentifier, sendNoteOnFor);
+  setLocalNamespace(obj);
 
 };
 
 
 void MIDIController::newJsFileLoaded(){
-    scriptPath->setValue(currentFile.getFullPathName());
+  scriptPath->setValue(currentFile.getFullPathName());
 };
 
 // @ben do we do the same as OSC MIDI IN/OUt controllers
 
 var MIDIController::sendCC(const var::NativeFunctionArgs &){
-    //    MIDIController * c = getObjectPtrFromJS<MIDIController>(a);
+  //    MIDIController * c = getObjectPtrFromJS<MIDIController>(a);
 
 
 
-    return var::undefined();
+  return var::undefined();
 }
 var MIDIController::sendNoteOnFor(const var::NativeFunctionArgs &){
 
-    return var::undefined();
+  return var::undefined();
 }
