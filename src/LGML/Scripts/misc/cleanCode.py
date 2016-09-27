3a2de9effb5ca3e223440c04b444d3d7789f93c0
Martin Hermant <martin.hermant@gmail.com>
2016-09-27 16:41:16 +0200

    clean cleanCode.py

--- src/LGML/Scripts/misc/cleanCode.py
+++ src/LGML/Scripts/misc/cleanCode.py
@@ -1,75 +1,13 @@
-lines = ["DummyNodeContentUI.h:15: 'DummyNode.h' does not need to be #included; use a forward declaration instead",
-"IntSliderUI.h:15: 'IntParameter.h' does not need to be #included",
-"JavascriptControllerEditor.h:15: 'JavascriptControllerUI.h' does not need to be #included; use a forward declaration instead",
-"JavascriptControllerUI.h:15: 'JavascriptController.h' does not need to be #included; use a forward declaration instead",
-"JsEnvironmentUI.h:15: 'DebugHelpers.h' does not need to be #included",
-"JsGlobalEnvironment.h:15: 'JsHelpers.h' does not need to be #included",
-"JsLiveEditor.h:15: 'JsEnvironment.h' does not need to be #included",
-"JsNode.h:16: 'DebugHelpers.h' does not need to be #included",
-"JsNodeUI.h:15: 'NodeBaseUI.h' does not need to be #included",
-"LGMLLoggerUI.h:17: 'Style.h' does not need to be #included",
-"LookAndFeelOO.h:18: 'Style.h' does not need to be #included",
-"LooperNode.h:16: 'AudioHelpers.h' does not need to be #included",
-"LooperNodeUI.h:14: 'NodeBaseUI.h' does not need to be #included",
-"LooperTrack.h:15: 'RingBuffer.h' does not need to be #included",
-"LooperTrack.h:16: 'AudioHelpers.h' does not need to be #included",
-"MIDIControllerEditor.h:15: 'MIDIControllerUI.h' does not need to be #included; use a forward declaration instead",
-"MainComponent.h:13: 'Inspector.h' does not need to be #included",
-"MainComponent.h:32: 'ShapeShifterManager.h' does not need to be #included",
-"MainComponent.h:33: 'Engine.h' does not need to be #included; use a forward declaration instead",
-"NodeBase.h:22: 'ControllableContainer.h' does not need to be #included",
-"NodeBase.h:23: 'PresetManager.h' does not need to be #included",
-"NodeBaseUI.h:14: 'Style.h' does not need to be #included",
-"NodeBaseUI.h:17: 'NodeBaseContentUI.h' does not need to be #included; use a forward declaration instead",
-"NodeBaseUI.h:18: 'NodeBaseHeaderUI.h' does not need to be #included; use a forward declaration instead",
-"NodeConnection.h:15: 'DataProcessorGraph.h' does not need to be #included; use a forward declaration instead",
-"NodeConnectionEditor.h:19: 'NodeConnectionUI.h' does not need to be #included; use a forward declaration instead",
-"NodeConnectionEditorDataSlot.h:17: 'NodeBaseUI.h' does not need to be #included",
-"NodeConnectionEditorLink.h:15: 'NodeConnectionEditorDataSlot.h' does not need to be #included; use a forward declaration instead",
-"NodeConnectionUI.h:15: 'NodeBaseUI.h' does not need to be #included",
-"NodeContainer.h:19: 'ContainerInNode.h' does not need to be #included; use a forward declaration instead",
-"NodeContainer.h:20: 'ContainerOutNode.h' does not need to be #included; use a forward declaration instead",
-"NodeContainerContentUI.h:15: 'ParameterProxy.h' does not need to be #included; use a forward declaration instead",
-"NodeContainerUI.h:14: 'NodeContainer.h' does not need to be #included; use a forward declaration instead",
-"NodeContainerUI.h:15: 'InspectableComponent.h' does not need to be #included",
-"NodeManager.h:19: 'NodeConnection.h' does not need to be #included",
-"NodeManager.h:20: 'NodeFactory.h' does not need to be #included",
-"OSCController.h:15: 'StringParameter.h' does not need to be #included; use a forward declaration instead",
-"OSCControllerEditor.h:14: 'OSCControllerUI.h' does not need to be #included; use a forward declaration instead",
-"OSCControllerUI.h:15: 'OSCController.h' does not need to be #included; use a forward declaration instead",
-"OSCCustomControllerEditor.h:15: 'OSCCustomControllerUI.h' does not need to be #included; use a forward declaration instead",
-"OSCCustomControllerUI.h:15: 'OSCCustomController.h' does not need to be #included; use a forward declaration instead",
-"PlayableBuffer.h:15: 'DebugHelpers.h' does not need to be #included",
-"RuleCondition.h:16: 'Rule.h' does not need to be #included; use a forward declaration instead",
-"RuleConditionGroup.h:14: 'Rule.h' does not need to be #included; use a forward declaration instead",
-"RuleConditionGroupUI.h:15: 'RuleConditionGroup.h' does not need to be #included; use a forward declaration instead",
-"RuleConditionUI.h:14: 'RuleCondition.h' does not need to be #included; use a forward declaration instead",
-"RuleConsequence.h:15: 'Rule.h' does not need to be #included; use a forward declaration instead",
-"RuleConsequenceUI.h:15: 'RuleConsequence.h' does not need to be #included; use a forward declaration instead",
-"RuleEditor.h:16: 'RuleUI.h' does not need to be #included; use a forward declaration instead",
-"RuleEditor.h:17: 'RuleConditionUI.h' does not need to be #included",
-"RuleUI.h:14: 'Rule.h' does not need to be #included; use a forward declaration instead",
-"ScriptedConditionUI.h:14: 'ScriptedCondition.h' does not need to be #included; use a forward declaration instead",
-"ScriptedConsequenceUI.h:15: 'ScriptedConsequence.h' does not need to be #included; use a forward declaration instead",
-"SelectableComponent.h:19: 'ControllableContainer.h' does not need to be #included; use a forward declaration instead",
-"SerialControllerEditor.h:16: 'SerialControllerUI.h' does not need to be #included; use a forward declaration instead",
-"SerialControllerUI.h:15: 'SerialController.h' does not need to be #included; use a forward declaration instead",
-"ShapeShifterPanel.h:15: 'ShapeShifterPanelTab.h' does not need to be #included; use a forward declaration instead",
-"ShapeShifterPanelTab.h:14: 'ShapeShifterContent.h' does not need to be #included; use a forward declaration instead",
-"StringParameterUI.h:16: 'StringParameter.h' does not need to be #included",
-"TimeManager.h:21: 'NodeBase.h' does not need to be #included",
-"TimeManagerUI.h:14: 'TimeManager.h' does not need to be #included; use a forward declaration instead",
-"TimeManagerUI.h:15: 'FloatStepperUI.h' does not need to be #included",
-"VSTNode.h:15: 'VSTManager.h' does not need to be #included",
-"VSTNode.h:18: 'PluginWindow.h' does not need to be #included",
-"VSTNode.h:19: 'TimeManager.h' does not need to be #included",
-"VSTNodeUI.h:16: 'NodeBaseUI.h' does not need to be #included",
-"VuMeter.h:15: 'Style.h' does not need to be #included"]
+# this script analyses code and remove useless dependencies (can not be fully trusted, but still useful )
+# if one want to force dependency to stay the line has to end by "//keep"
+
 
 import os,copy
 
-
-
+# globalOptions
+globalIgnore = [""]
+showIgnored = True
+dryRun = True;
 
 
 rootPath = "../../Source/"
@@ -105,9 +43,7 @@ for l in linesNoNeedInclude:
 print processedFiles
 
 
-globalIgnore = [""]
-showIgnored = True
-dryRun = True;
+
 
 for file,linesToChange in processedFiles.iteritems():
 	with open(os.path.abspath(rootPath+file),'r') as f:
