17fd94f5da1448e36c018ff2c0855663f496dc6a
Martin Hermant <martin.hermant@gmail.com>
2016-10-05 02:08:16 +0200

    redo bailaout check by default

--- src/LGML/Source/GitSha.h
+++ src/LGML/Source/GitSha.h
@@ -1,4 +1,4 @@
 #pragma once
-#define GIT_SHA  "7e097fb59464d3e49706befb6c024797cd4e8cc6"
-#define GIT_SHA_SHORT  "7e097fb"
-#define COMMIT_DATE  "05/10/16 (02:00)"
+#define GIT_SHA  "38cc6040b0875e8b7e4bb71df150af63380f69e5"
+#define GIT_SHA_SHORT  "38cc604"
+#define COMMIT_DATE  "05/10/16 (02:08)"
