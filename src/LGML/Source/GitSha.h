89f454489c0d16f4d04ae9ce5bde7d4c602ace06
Martin Hermant <martin.hermant@gmail.com>
2016-10-20 13:30:21 +0200

    remove Clear JSEnvironment between session

--- src/LGML/Source/GitSha.h
+++ src/LGML/Source/GitSha.h
@@ -1,4 +1,4 @@
 #pragma once
-#define GIT_SHA  "7e5cf2768fee5f81379e6134aee4022fe0eac003"
-#define GIT_SHA_SHORT  "7e5cf27"
-#define COMMIT_DATE  "20/10/16 (13:27)"
+#define GIT_SHA  "47f6531b62b6a73975eac32be8bbbcc2b013ece3"
+#define GIT_SHA_SHORT  "47f6531"
+#define COMMIT_DATE  "20/10/16 (13:30)"
