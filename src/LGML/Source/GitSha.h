7f946ce9076cf4fa15ce36f313ee26f24a52dcaf
Martin Hermant <martin.hermant@gmail.com>
2016-10-22 17:57:42 +0200

    fix override / no-return function warning

--- src/LGML/Source/GitSha.h
+++ src/LGML/Source/GitSha.h
@@ -1,4 +1,4 @@
 #pragma once
-#define GIT_SHA  "47f6531b62b6a73975eac32be8bbbcc2b013ece3"
-#define GIT_SHA_SHORT  "47f6531"
-#define COMMIT_DATE  "20/10/16 (13:30)"
+#define GIT_SHA  "20e493137b9fa2b8ef01a49d13460557c8ff49cc"
+#define GIT_SHA_SHORT  "20e4931"
+#define COMMIT_DATE  "22/10/16 (17:57)"
