abc96eec5c3685ae491203c10be4c2ab4e236088
Ben Kuper <bkuperberg@gmail.com>
2017-01-11 15:55:43 +0100

    Update 1.1.5 develop and OSX Unified global Paths

--- src/LGML/Source/GitSha.h
+++ src/LGML/Source/GitSha.h
@@ -1,4 +1,4 @@
 #pragma once
-#define GIT_SHA  "a6d6888c2b885952a23f92a97260fab1aa5acd7a"
-#define GIT_SHA_SHORT  "a6d6888"
-#define COMMIT_DATE  "06/12/16 (15:17)"
+#define GIT_SHA  "eb10dcbe6d0bf81d1527bb8ac5f5c6bbe5bf1f59"
+#define GIT_SHA_SHORT  "eb10dcb"
+#define COMMIT_DATE  "11/01/17 (15:53)"
