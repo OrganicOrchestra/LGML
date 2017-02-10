7888d6937c9ca3d156007da8426cd32aff656637
Martin Hermant <martin.hermant@gmail.com>
2017-02-10 12:51:34 -0400

    v1.1.6beta update juce 4.3.1

--- src/LGML/JuceLibraryCode/JuceHeader.h
+++ src/LGML/JuceLibraryCode/JuceHeader.h
@@ -42,8 +42,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "LGML";
-    const char* const  versionString  = "1.1.4beta";
-    const int          versionNumber  = 0x10104;
+    const char* const  versionString  = "1.1.6beta";
+    const int          versionNumber  = 0x10106;
 }
 #endif
 
