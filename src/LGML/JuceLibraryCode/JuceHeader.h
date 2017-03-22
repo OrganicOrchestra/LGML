e43f99efca56f362c290302a69fcbb4162fe1ed4
Martin Hermant <martin.hermant@gmail.com>
2017-03-22 19:04:05 +0100

    add correct juceHeader

--- src/LGML/JuceLibraryCode/JuceHeader.h
+++ src/LGML/JuceLibraryCode/JuceHeader.h
@@ -42,8 +42,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "LGML";
-    const char* const  versionString  = "1.1.7beta";
-    const int          versionNumber  = 0x10107;
+    const char* const  versionString  = "1.1.8beta";
+    const int          versionNumber  = 0x10108;
 }
 #endif
 
