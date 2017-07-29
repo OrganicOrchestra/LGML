f4b72ea8da00661aecdfe095f653365849abb988
Martin Hermant <martin.hermant@gmail.com>
2017-07-29 14:11:38 -0400

    LoggerUI : cleaner logger resizng

--- src/LGML/Source/LGMLLoggerUI.h
+++ src/LGML/Source/LGMLLoggerUI.h
@@ -1,12 +1,12 @@
 /*
-  ==============================================================================
+ ==============================================================================
 
-    LGMLLoggerUI.h
-    Created: 6 May 2016 2:13:35pm
-    Author:  Martin Hermant
+ LGMLLoggerUI.h
+ Created: 6 May 2016 2:13:35pm
+ Author:  Martin Hermant
 
-  ==============================================================================
-*/
+ ==============================================================================
+ */
 
 #ifndef LGMLLOGGERUI_H_INCLUDED
 #define LGMLLOGGERUI_H_INCLUDED
@@ -16,61 +16,61 @@
 #include "DebugHelpers.h"
 
 class LGMLLoggerUI : public ShapeShifterContentComponent,
-	public LGMLLogger::Listener,
-	public TextButton::Listener,
+public LGMLLogger::Listener,
+public TextButton::Listener,
 public AsyncUpdater
 {
 public:
-	LGMLLoggerUI(const String &contentName, LGMLLogger * l);
-	~LGMLLoggerUI();
+  LGMLLoggerUI(const String &contentName, LGMLLogger * l);
+  ~LGMLLoggerUI();
 
-    class LogList : public juce::TableListBoxModel
-	{
-    public:
-		LogList(LGMLLoggerUI * o);
+  class LogList : public juce::TableListBoxModel
+  {
+  public:
+    LogList(LGMLLoggerUI * o);
 
-        int getNumRows() override;
+    int getNumRows() override;
 
-        void paintRowBackground (Graphics&,
-                                         int rowNumber,
-                                         int width, int height,
-                                         bool rowIsSelected) override;
+    void paintRowBackground (Graphics&,
+                             int rowNumber,
+                             int width, int height,
+                             bool rowIsSelected) override;
 
-         void paintCell (Graphics&,
-                                int rowNumber,
-                                int columnId,
-                                int width, int height,
-                                bool rowIsSelected) override;
+    void paintCell (Graphics&,
+                    int rowNumber,
+                    int columnId,
+                    int width, int height,
+                    bool rowIsSelected) override;
     String getCellTooltip (int /*rowNumber*/, int /*columnId*/)    override;
 
-    private:
+  private:
 
 
-        LGMLLoggerUI * owner;
-    };
+    LGMLLoggerUI * owner;
+  };
 
-	LGMLLogger * logger;
+  LGMLLogger * logger;
 
-    void resized()override;
-    LogList logList;
-    TextButton clearB;
-    ScopedPointer<TableListBox> logListComponent;
-    int maxNumElement;
+  void resized()override;
+  LogList logList;
+  TextButton clearB;
+  ScopedPointer<TableListBox> logListComponent;
+  int maxNumElement;
 
-    void buttonClicked (Button*) override;
+  void buttonClicked (Button*) override;
 
 
-    OwnedArray<LogElement> logElements;
-    void newMessage(const String & ) override;
+  OwnedArray<LogElement> logElements;
+  void newMessage(const String & ) override;
 private:
-    int totalLogRow;
-    void updateTotalLogRow();
-    const String & getSourceForRow(int r);
-    const String & getContentForRow(int r);
-	String getTimeStringForRow(int r);
-    friend class LogList;
+  int totalLogRow;
+  void updateTotalLogRow();
+  const String & getSourceForRow(int r);
+  const String & getContentForRow(int r);
+  String getTimeStringForRow(int r);
+  friend class LogList;
 
-    void handleAsyncUpdate()override;
+  void handleAsyncUpdate()override;
 
 };
 
