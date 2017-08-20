51d25db767ca1fcd9c419066f75dd4e78ac350e5
Martin Hermant <martin.hermant@gmail.com>
2017-08-20 13:35:19 -0400

    cci : * add artifacts * try to remove unnecessary libs * ldd debug

--- Scripts/linux/installDevDeps.sh
+++ Scripts/linux/installDevDeps.sh
@@ -6,17 +6,18 @@ if [ -z ${LGML_TARGET_ARCH+x} ]; then LGML_TARGET_ARCH=`dpkg --print-architectur
 apt-get -y --force-yes install libavahi-compat-libdnssd-dev
 
 ## these are devloper libs needed for JUCE,   not sure wich are needed in released version...
+# from Makefile alsa freetype2 libcurl x11 xext xinerama
 apt-get -y --force-yes install libfreetype6-dev:$LGML_TARGET_ARCH
 apt-get -y --force-yes install libx11-dev:$LGML_TARGET_ARCH
 apt-get -y --force-yes install libxinerama-dev:$LGML_TARGET_ARCH
-apt-get -y --force-yes install libxrandr-dev:$LGML_TARGET_ARCH
-apt-get -y --force-yes install libxcursor-dev:$LGML_TARGET_ARCH
-apt-get -y --force-yes install mesa-common-dev:$LGML_TARGET_ARCH
+# apt-get -y --force-yes install libxrandr-dev:$LGML_TARGET_ARCH
+# apt-get -y --force-yes install libxcursor-dev:$LGML_TARGET_ARCH
+# apt-get -y --force-yes install mesa-common-dev:$LGML_TARGET_ARCH
 apt-get -y --force-yes install libasound2-dev:$LGML_TARGET_ARCH
-apt-get -y --force-yes install freeglut3-dev:$LGML_TARGET_ARCH
+# apt-get -y --force-yes install freeglut3-dev:$LGML_TARGET_ARCH
 apt-get -y --force-yes install libxcomposite-dev:$LGML_TARGET_ARCH
 apt-get -y --force-yes install libjack-dev:$LGML_TARGET_ARCH
-
+apt-get -y --force-yes install libcurl-dev:$LGML_TARGET_ARCH
 
 SCRIPTPATH=`pwd`/$(dirname "$0") 
 cd $SCRIPTPATH
@@ -25,7 +26,7 @@ cd ../../..
 pwd
 ls
 
-apt-get -y --force-yes install curl
+
 apt-get -y --force-yes install unzip
 
 if [ ! -d "JUCE" ]; then
