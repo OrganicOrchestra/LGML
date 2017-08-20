71a38e8fbc10a1f24dd981aaab70846d008d7d17
Martin Hermant <martin.hermant@gmail.com>
2017-08-20 16:14:35 -0400

    cci : back to custom arch flag

--- Scripts/linux/installDevDeps.sh
+++ Scripts/linux/installDevDeps.sh
@@ -3,21 +3,21 @@
 if [ -z ${LGML_TARGET_ARCH+x} ]; then LGML_TARGET_ARCH=`dpkg --print-architecture`; else echo "arch is set to '$LGML_TARGET_ARCH'"; fi
 
 # for dns utility
-apt-get -y -q --force-yes install libavahi-compat-libdnssd-dev
+apt-get -y --force-yes install libavahi-compat-libdnssd-dev
 
 ## these are devloper libs needed for JUCE,   not sure wich are needed in released version...
 # from Makefile alsa freetype2 libcurl x11 xext xinerama
-apt-get -y -q --force-yes install libfreetype6-dev
-apt-get -y -q --force-yes install libx11-dev
-apt-get -y -q --force-yes install libxinerama-dev
-apt-get -y -q --force-yes install libxrandr-dev
-apt-get -y -q --force-yes install libxcursor-dev
-apt-get -y -q --force-yes install mesa-common-dev
-apt-get -y -q --force-yes install libasound2-dev
-apt-get -y -q --force-yes install freeglut3-dev
-apt-get -y -q --force-yes install libxcomposite-dev
-apt-get -y -q --force-yes install libjack-dev
-apt-get -y -q --force-yes install libcurl4-gnutls-dev
+apt-get -y --force-yes install libfreetype6-dev:$LGML_TARGET_ARCH
+apt-get -y --force-yes install libx11-dev:$LGML_TARGET_ARCH
+apt-get -y --force-yes install libxinerama-dev:$LGML_TARGET_ARCH
+apt-get -y --force-yes install libxrandr-dev:$LGML_TARGET_ARCH
+apt-get -y --force-yes install libxcursor-dev:$LGML_TARGET_ARCH
+apt-get -y --force-yes install mesa-common-dev:$LGML_TARGET_ARCH
+apt-get -y --force-yes install libasound2-dev:$LGML_TARGET_ARCH
+apt-get -y --force-yes install freeglut3-dev:$LGML_TARGET_ARCH
+apt-get -y --force-yes install libxcomposite-dev:$LGML_TARGET_ARCH
+apt-get -y --force-yes install libjack-dev:$LGML_TARGET_ARCH
+apt-get -y --force-yes install libcurl4-gnutls-dev:$LGML_TARGET_ARCH
 
 SCRIPTPATH=`pwd`/$(dirname "$0") 
 cd $SCRIPTPATH
