6ddec6c6bd91b87a105e084224890bf6a5db71fc
Martin Hermant <martin.hermant@gmail.com>
2017-08-20 15:43:37 -0400

    remove target arch

--- Scripts/linux/installDeps.sh
+++ Scripts/linux/installDeps.sh
@@ -5,6 +5,6 @@
 #sudo apt-get -y install libcurl4-gnutls-dev
 if [ -z ${LGML_TARGET_ARCH+x} ]; then LGML_TARGET_ARCH=`dpkg --print-architecture`; else echo "arch is set to '$LGML_TARGET_ARCH'"; fi
 # rubberband
-apt-get -y --force-yes install librubberband-dev:$LGML_TARGET_ARCH
+apt-get -y --force-yes install librubberband-dev
 
 
--- Scripts/linux/installDevDeps.sh
+++ Scripts/linux/installDevDeps.sh
@@ -3,21 +3,21 @@
 if [ -z ${LGML_TARGET_ARCH+x} ]; then LGML_TARGET_ARCH=`dpkg --print-architecture`; else echo "arch is set to '$LGML_TARGET_ARCH'"; fi
 
 # for dns utility
-apt-get -y --force-yes install libavahi-compat-libdnssd-dev
+apt-get -y -q --force-yes install libavahi-compat-libdnssd-dev
 
 ## these are devloper libs needed for JUCE,   not sure wich are needed in released version...
 # from Makefile alsa freetype2 libcurl x11 xext xinerama
-apt-get -y --force-yes install libfreetype6-dev:$LGML_TARGET_ARCH
-apt-get -y --force-yes install libx11-dev:$LGML_TARGET_ARCH
-apt-get -y --force-yes install libxinerama-dev:$LGML_TARGET_ARCH
-apt-get -y --force-yes install libxrandr-dev:$LGML_TARGET_ARCH
-apt-get -y --force-yes install libxcursor-dev:$LGML_TARGET_ARCH
-apt-get -y --force-yes install mesa-common-dev:$LGML_TARGET_ARCH
-apt-get -y --force-yes install libasound2-dev:$LGML_TARGET_ARCH
-apt-get -y --force-yes install freeglut3-dev:$LGML_TARGET_ARCH
-apt-get -y --force-yes install libxcomposite-dev:$LGML_TARGET_ARCH
-apt-get -y --force-yes install libjack-dev:$LGML_TARGET_ARCH
-apt-get -y --force-yes install libcurl4-gnutls-dev:$LGML_TARGET_ARCH
+apt-get -y -q --force-yes install libfreetype6-dev
+apt-get -y -q --force-yes install libx11-dev
+apt-get -y -q --force-yes install libxinerama-dev
+apt-get -y -q --force-yes install libxrandr-dev
+apt-get -y -q --force-yes install libxcursor-dev
+apt-get -y -q --force-yes install mesa-common-dev
+apt-get -y -q --force-yes install libasound2-dev
+apt-get -y -q --force-yes install freeglut3-dev
+apt-get -y -q --force-yes install libxcomposite-dev
+apt-get -y -q --force-yes install libjack-dev
+apt-get -y -q --force-yes install libcurl4-gnutls-dev
 
 SCRIPTPATH=`pwd`/$(dirname "$0") 
 cd $SCRIPTPATH
@@ -27,8 +27,8 @@ pwd
 ls
 
 
-apt-get -y --force-yes install unzip
-apt-get -y --force-yes install curl
+apt-get -y -q --force-yes install unzip
+apt-get -y -q --force-yes install curl
 
 if [ ! -d "JUCE" ]; then
   curl -L https://github.com/julianstorer/JUCE/archive/master.zip > JUCE.zip
@@ -37,9 +37,9 @@ if [ ! -d "JUCE" ]; then
 fi
 
 
-apt-get -y --force-yes install python
+apt-get -y -q --force-yes install python
 
-apt-get -y --force-yes install libcurl4-openssl-dev
+apt-get -y -q --force-yes install libcurl4-openssl-dev
 # cd ;
 # if [ ! -d "Dev/Projucer/linux" ]; then
 #   cd 
