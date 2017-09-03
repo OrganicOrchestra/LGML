99dd0dc280a1257feaf863ba14bfeed277778e89
Martin Hermant <martin.hermant@gmail.com>
2017-09-03 18:43:13 -0400

    bump to 1.2.2

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -41,7 +41,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "LGML";
-    const char* const  versionString  = "1.2.0-beta";
-    const int          versionNumber  = 0x10200;
+    const char* const  versionString  = "1.2.2-beta";
+    const int          versionNumber  = 0x10202;
 }
 #endif
