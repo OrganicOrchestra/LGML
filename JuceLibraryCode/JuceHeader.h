11a90ee092f36e036143ba10139e6fac854592b9
Martin Hermant <martin.hermant@gmail.com>
2017-09-03 20:20:33 -0400

    remove beta from version number use standard semver from now on

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -41,7 +41,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "LGML";
-    const char* const  versionString  = "1.2.2-beta";
-    const int          versionNumber  = 0x10202;
+    const char* const  versionString  = "1.2.3";
+    const int          versionNumber  = 0x10203;
 }
 #endif
