8ef474f53bd5dd285ec8af07485a18f81f89f4f6
Martin Hermant <martin.hermant@gmail.com>
2017-11-17 19:43:08 +0100

    travis: update export deps to py3

--- Scripts/osx/installExportDeps.sh
+++ Scripts/osx/installExportDeps.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/bash
 
-pip install -U pip; 
-pip install dmgbuild;
-pip install requests;
+
+
+if command -v python3 &>/dev/null; then
+    echo Python 3 is installed
+else
+    #install brew if needed
+    if command -v brew &/dev/null; then
+      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+    brew install python3
+fi
+
+
+pip3 install -U pip3; 
+pip3 install dmgbuild;
+pip3 install requests;
