01a67bf4c79ee03251041d0c93c0ba1b0a40f7a9
Martin Hermant <martin.hermant@gmail.com>
2017-11-27 14:55:17 +0100

    Scripts : shutils.py add a detached mode

--- Scripts/PyUtils/shUtils.py
+++ Scripts/PyUtils/shUtils.py
@@ -1,20 +1,21 @@
 import os;
 import subprocess
 
-def sh(cmd,printIt=True):
-	def inSH(cmd):
+def sh(cmd,printIt=True,detach=False):
+	def inSH(cmd,detach):
 		if printIt : print(("exec : "+cmd));
 		res =  subprocess.Popen(cmd, shell=True,stdout=subprocess.PIPE, universal_newlines=True)
-		stdout_lines = iter(res.stdout.readline, "")
-		for stdout_line in stdout_lines:
-			yield stdout_line
+		if(not detach):
+			stdout_lines = iter(res.stdout.readline, "")
+			for stdout_line in stdout_lines:
+				yield stdout_line
 
-		res.stdout.close()
-		return_code = res.wait()
-		if return_code != 0:
-			raise subprocess.CalledProcessError(return_code, cmd)
+			res.stdout.close()
+			return_code = res.wait()
+			if return_code != 0:
+				raise subprocess.CalledProcessError(return_code, cmd)
 	res = ""
-	for l in inSH(cmd):
+	for l in inSH(cmd,detach):
 		if l != '\n':
 			res+=l
 			if(printIt): print(l)
