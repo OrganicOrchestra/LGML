7dff60d875d1aab28ce13ea416b26b079dd85876
Martin Hermant <martin.hermant@gmail.com>
2017-12-08 10:21:03 +0100

    Scripts : fix git sha when called from submodule

--- Scripts/PyUtils/gitUtils.py
+++ Scripts/PyUtils/gitUtils.py
@@ -12,11 +12,17 @@ gitShaPath = os.path.dirname(os.path.abspath(__file__));
 gitShaPath = os.path.join(gitShaPath,"../../Source/GitSha.h");
 gitShaPath = os.path.abspath(gitShaPath);
 
+LGMLDir = os.path.dirname(os.path.abspath(__file__));
+LGMLDir = os.path.join(LGMLDir,os.pardir,os.pardir);
 
 
-
-def getGitSha(short = True):
-	sha= sh("git rev-parse --verify HEAD",printIt=False)[:-1].strip()
+def getGitSha(short = True,globalSha = True):
+	if globalSha:
+		oldDir = os.getcwd()
+		os.chdir(LGMLDir)
+	sha= sh('git rev-parse --verify HEAD',printIt=False)[:-1].strip()
+	if globalSha:
+		os.chdir(oldDir)
 	if(short):
 		sha = sha[0:8]
 	return sha
