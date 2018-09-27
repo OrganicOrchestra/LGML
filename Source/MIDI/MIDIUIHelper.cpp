6fadee323fb04938ebd83aa44fe298533e7c9756
Martin Hermant <martin.hermant@gmail.com>
2018-09-27 18:06:57 +0200

    change entete

--- Source/MIDI/MIDIUIHelper.cpp
+++ Source/MIDI/MIDIUIHelper.cpp
@@ -3,7 +3,7 @@
 
  Copyright © Organic Orchestra, 2017
 
- This file is part of LGML. LGML is a software to manipulate sound in realtime
+ This file is part of LGML. LGML is a software to manipulate sound in real-time
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -15,7 +15,6 @@
 
  ==============================================================================
  */
-
 
 #if !ENGINE_HEADLESS
 #include "MIDIUIHelper.h"
--- Source/Utils/FactoryUIHelpers.cpp
+++ Source/Utils/FactoryUIHelpers.cpp
@@ -1,12 +1,20 @@
 /*
-  ==============================================================================
+ ==============================================================================
 
-    FactoryUIHelpers.cpp
-    Created: 7 Mar 2018 12:26:25pm
-    Author:  Martin Hermant
+ Copyright © Organic Orchestra, 2017
 
-  ==============================================================================
-*/
+ This file is part of LGML. LGML is a software to manipulate sound in real-time
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation (version 3 of the License).
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ ==============================================================================
+ */
 
 #if !ENGINE_HEADLESS
 
