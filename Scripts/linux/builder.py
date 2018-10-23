a515b46cccc966cb12821b6a3f2a6098862f36ac
Martin Hermant <martin.hermant@gmail.com>
2018-10-23 21:15:22 +0200

    linux buid script: fix error parsing gcc version

--- Scripts/linux/builder.py
+++ Scripts/linux/builder.py
@@ -92,7 +92,7 @@ class LinuxBuilder (BuilderBase):
 
     gccv = sh("gcc -dumpversion")
     gccvnum  =list( map(lambda x:int(x),gccv.split('.')))
-    print ("gcc version : %i,%i"%(gccvnum[0],gccvnum[1]))
+    print ("gcc version : %i"%(gccvnum))
     if (gccvnum[0]<5 and ((len(gccvnum)<=1) or gccvnum[1]<9 ) ):
       print ("patching makefile : to use c++11 (experiemental)")
       patchMakeFile("c++14","c++11")
