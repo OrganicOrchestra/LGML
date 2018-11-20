600772883b9f03e5753cccf2627fa7509fdfed4a
Tintamar <martin.hermant@gmail.com>
2018-11-21 00:12:45 +0100

    linux CI remove need for user input

--- Scripts/linux/installDevDeps.sh
+++ Scripts/linux/installDevDeps.sh
@@ -88,7 +88,7 @@ apt-get -y -q --assume-yes install libavahi-compat-libdnssd-dev:$CROSS_ARCH
 apt-get  -y -q --assume-yes  install software-properties-common python-software-properties
 
 # install cmake
-add-apt-repository ppa:george-edison55/cmake-3.x
+add-apt-repository ppa:george-edison55/cmake-3.x -y
 apt-get update
 apt-get -y -q --assume-yes install cmake
 
