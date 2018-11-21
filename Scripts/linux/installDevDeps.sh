d8b15df5900bb6093527e9f6b4f2122ea840cb5b
Tintamar <martin.hermant@gmail.com>
2018-11-21 09:46:14 +0100

    linux CI : try to remove python-softwar BS

--- Scripts/linux/installDevDeps.sh
+++ Scripts/linux/installDevDeps.sh
@@ -85,7 +85,7 @@ apt-get -y -q --assume-yes install libavahi-compat-libdnssd-dev:$CROSS_ARCH
 
 #install add-apt-repository
 
-apt-get  -y -q --assume-yes  install software-properties-common python-software-properties
+apt-get  -y -q --assume-yes  install software-properties-common
 
 # install cmake
 add-apt-repository ppa:george-edison55/cmake-3.x -y
