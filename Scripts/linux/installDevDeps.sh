f0600e767f05ddec093fe193e217567f90fc536a
Tintamar <martin.hermant@gmail.com>
2018-11-21 10:00:25 +0100

    appveyor try with camomille flags

--- Scripts/linux/installDevDeps.sh
+++ Scripts/linux/installDevDeps.sh
@@ -94,7 +94,7 @@ apt-get -y -q --assume-yes install cmake
 
 cd LGML/third_party/libpd
 mkdir build && cd build
-CFLAGS="$ARCH_FLAGS" LDFLAGS="$ARCH_FLAGS" cmake .. -DPD_MULTI:BOOL=ON 
+CFLAGS="$ARCH_FLAGS" LDFLAGS="$ARCH_FLAGS" cmake .. -DPD_MULTI:BOOL=ON  -DPD_UTILS:BOOL=OFF
 cmake --build .
 
 # cd ;
