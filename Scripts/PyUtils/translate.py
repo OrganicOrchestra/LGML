9e59d8b06a008c85442fe0024518192c1b099313
Tintamar <martin.hermant@gmail.com>
2018-12-01 13:27:54 +0100

    Translation use MicrosoftAzure and fix bugs with line jumps

--- Scripts/PyUtils/translate.py
+++ Scripts/PyUtils/translate.py
@@ -7,7 +7,7 @@ import re
 import json
 
 sourcePath = os.path.abspath(os.path.join(__file__,os.path.pardir,os.path.pardir,os.path.pardir,'Source'))
-jucePath = os.path.abspath(os.path.join(__file__,os.path.pardir,os.path.pardir,os.path.pardir,os.path.pardir,'JUCE','modules'))
+jucePath =  "/tmp/la/"#os.path.abspath(os.path.join(__file__,os.path.pardir,os.path.pardir,os.path.pardir,os.path.pardir,'JUCE','modules'))
 print (sourcePath,jucePath)
 baseTranslationPath = os.path.expanduser('~/Documents/LGML/translations/')
 # baseTranslationPath = os.path.expanduser('~/owncloud/DEVSPECTACLES/Tools/LGML/translations')
@@ -23,30 +23,45 @@ def getFileList():
   return files
 
 
-def getEnclosed(s,start,opc='(',clc=')',ignore_escaped=False):
-  starti = -1;
-  i=start-1
-  depth = 0
-  wasEscaped = False
-  for c in s[start:]:
-    i+=1
-    if wasEscaped:
-      wasEscaped=False
-      continue
-    if c==clc and depth>0:
-      depth-=1
-    elif c==opc:
-      depth+=1
-      if starti<0 :
-        starti = i;
-    elif ignore_escaped and c=='\\':
-      wasEscaped = True
+# def getEnclosed(s,start,opc='(',clc=')',ignore_escaped=False):
+#   starti = -1;
+#   i=start-1
+#   depth = 0
+#   wasEscaped = False
+#   for c in s[start:]:
+#     i+=1
+#     if wasEscaped:
+#       wasEscaped=False
+#       continue
+#     if c==clc and depth>0:
+#       depth-=1
+#     elif c==opc:
+#       depth+=1
+#       if starti<0 :
+#         starti = i;
+#     elif ignore_escaped and c=='\\':
+#       wasEscaped = True
 
-    if starti>=0 and depth==0:
-      return (starti,i)
-  return None
+#     if starti>=0 and depth==0:
+#       return (starti,i)
+#   return None
 
 
+
+def buildRegFunction(fname,valid_mask,strict = True):
+  anys = r"[\s\n\r]*"
+  s = r'(?<!//)'+anys+fname+anys+"\("+anys
+  for m in  valid_mask:
+    if m:
+      s+=r'(?:String'+anys+'\(|)\"(.+?)\"'
+    else:
+      s+=r'.+'
+    s+=anys+','+anys
+  s = s[:-len(anys)-1]
+  if(strict) :
+    s+=anys+"\)"
+  print(s)
+  return re.compile(s,re.MULTILINE)
 
 def getRegExs(fl):
   regL = []
@@ -93,68 +108,108 @@ def getRegExs(fl):
         # if m : print(m);
   return res
 
-def getAllStrings(fl):
-  valid_s = re.compile("([A-Za-z0-9\ ]+)")
-  digit_s = re.compile("([0-9\ ]+)")
-  res ={}
-  for f in fl:
-    if "test" in f.lower():
-      continue
-    # print('reading strings from %s'%f)
-    with open(f,'r',errors='replace',encoding='utf-8') as fp:
+# def getAllStrings(fl):
+#   valid_s = re.compile("([A-Za-z0-9\ ]+)")
+#   digit_s = re.compile("([0-9\ ]+)")
+#   res ={}
+#   for f in fl:
+#     if "test" in f.lower():
+#       continue
+#     # print('reading strings from %s'%f)
+#     with open(f,'r',errors='replace',encoding='utf-8') as fp:
 
-      i = 0
-      for l in fp.readlines():
-        i+=1
-        l = l.strip('/ ')
-        if "DBG" in l:
-          continue
-        found_idx = 0
-        sub = getEnclosed(l,found_idx,'"','"',True)
-        while sub:
-          innerT = l[sub[0]+1:sub[1]]
-          if re.match(valid_s,innerT) and len(innerT)>1 and innerT[-2:] != '.h' and not re.match(digit_s,innerT) and not '_' in innerT and not 'http' in innerT:
-            res[innerT]=(f,i)
-          found_idx=l.find('"',sub[1]+1)
-          if found_idx<0:
-            break;
+#       i = 0
+#       for l in fp.readlines():
+#         i+=1
+#         l = l.strip('/ ')
+#         if "DBG" in l:
+#           continue
+#         found_idx = 0
+#         sub = getEnclosed(l,found_idx,'"','"',True)
+#         while sub:
+#           innerT = l[sub[0]+1:sub[1]]
+#           if re.match(valid_s,innerT) and len(innerT)>1 and innerT[-2:] != '.h' and not re.match(digit_s,innerT) and not '_' in innerT and not 'http' in innerT:
+#             res[innerT]=(f,i)
+#           found_idx=l.find('"',sub[1]+1)
+#           if found_idx<0:
+#             break;
           
-          sub = getEnclosed(l,found_idx,'"','"',True)
+#           sub = getEnclosed(l,found_idx,'"','"',True)
           
 
 
-  return res
+#   return res
 
-def splitEscaped(s):
-  wasEscaped = False
-  cur = ""
-  ls = []
-  escaped = []
-  for c in s:
-    if wasEscaped:
-      escaped+=['\\'+c]
-      ls+=[cur]
-      cur=""
-      wasEscaped=False
-    elif c=='\\':
-      wasEscaped=True
-    else:
-      cur+=c
-  ls+=[cur]
-  return ls,escaped
+# def splitEscaped(s):
+#   wasEscaped = False
+#   cur = ""
+#   ls = []
+#   escaped = []
+#   for c in s:
+#     if wasEscaped:
+#       escaped+=['\\'+c]
+#       ls+=[cur]
+#       cur=""
+#       wasEscaped=False
+#     elif c=='\\':
+#       wasEscaped=True
+#     else:
+#       cur+=c
+#   ls+=[cur]
+#   return ls,escaped
 
-def mergeEscaped(ls,escaped):
-  res = ls[0]
-  for i in range(1,len(ls)):
-    res+=escaped[i-1]+ls[i]
-  return res
+# def mergeEscaped(ls,escaped):
+#   res = ls[0]
+#   for i in range(1,len(ls)):
+#     res+=escaped[i-1]+ls[i]
+#   return res
 
+
+# class YTranslator():
+#   baseAddr = "https://translate.yandex.net/api/v1.5/tr.json/translate"
+#   def translate(self,text,src,dest):
+#     import requests  
+#     r = requests.get(self.baseAddr,params={'key':os.environ["YTRANS_TOKEN"],'text':text,'lang':'%s-%s'%(src,dest)})
+#     print (r.json())
+#     return r.json()['text']
+
+# class GTranslator():
+#   baseAddr = 'https://translate.google.com/'
+
+#   def translate(self,text,src,dest):
+#     import requests
+#     r = requests.get(self.baseAddr,params={'um':1,'ie':'UTF-8','hl':dest,'client':'tw-ob','view':'home','op':'translate',
+#                                       'sl':src,'tl':dest,'text':text},
+#                                       headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+# )
+#     print (r.text)
+#     print (r.json())
+#     return r.json()['text']
+
+class MTranslator():
+  import uuid
+  base_url = 'https://api.cognitive.microsofttranslator.com'
+  path = '/translate?api-version=3.0'
+  headers = {
+    'Ocp-Apim-Subscription-Key': os.environ['AZURE_TRANS_TOKEN'],
+    'Content-type': 'application/json',
+    'X-ClientTraceId': str(uuid.uuid4())
+  }
+  maxNumChar = 2000
+  allowList = True
+  def translate(self,text,src,dest):
+    import requests
+    params = '&from'+src+'&to='+dest
+    constructed_url = self.base_url + self.path + params
+    jsonR = [{'Text' : ' \n '.join(k.split('\n'))} for k in text]
+    request = requests.post(constructed_url, headers=self.headers, json=jsonR)
+    return [k["translations"][0]['text'] for k in request.json()]
 
 
 def buildLocalMT(strs,locale='fr'):
   mtfile = os.path.join(baseTranslationPath,'mt.'+locale+'.json')
   from googletrans import Translator
-  translator = Translator()
+  translator = MTranslator()
   
   i = 0
   if os.path.exists(mtfile):
@@ -164,33 +219,76 @@ def buildLocalMT(strs,locale='fr'):
     mt = {}
 
   res = {}
+  
+
+  def doBatchTranslate(txtL,mt,res,strs):
+      if isinstance(txtL,str):
+        txtL = txtL.split('\n')
+
+      print('translating :%s '%(txtL))
+
+      # flatten \n s
+      itemsIdx =[]
+      flattenedL = []
+      for t in txtL:
+        flat = t.split('\\n')
+        flattenedL += flat
+        itemsIdx+=[len(flat)]
+      # print(itemsIdx,flattenedL)
+      trs = translator.translate(flattenedL,src='en',dest=locale)
+
+
+      if(len(trs)==1 and len(txtL)==1):
+        trs = trs[0]
+        trsL=trs.split('\n')
+      else:
+        idx = 0
+        idxOr = 0
+        trsL = []
+        while idxOr <len(txtL):
+          toJoin = itemsIdx[idxOr]
+          trsL+=['\\n'.join(trs[idx:idx+toJoin])]
+          idx+=toJoin
+          idxOr+=1
+      
+      print('trsL',trsL)
+      # print('textL',txtL)
+
+      assert(len(trsL)==len(txtL))
+      for i in range(len(txtL)):
+        kk = txtL[i] 
+        if(kk==''):
+          continue 
+        mt[kk]=trsL[i]
+        res[kk] = {'trans':mt[kk],'meta':strs[kk]}
+      with open(mtfile,'w') as fp:
+        json.dump(mt,fp)
+
+  txt=""
+  maxNumChars = translator.maxNumChar
   for k in strs:
-    if not k in mt:
-      print('translating :%s (%s,%i)'%(k,os.path.basename(strs[k][0]),strs[k][1]))
-      kl,esc = splitEscaped(k)
-      i+=1
-
-      trs = translator.translate(kl,src='en',dest=locale)
-      trs_l=[]
-      for t in trs:
-        trs_l += [t.text];#).decode('utf8')
-      trs_s = mergeEscaped(trs_l,esc)
-      mt[k]=trs_s
-      if(i%10 ==0):
-        with open(mtfile,'w') as fp:
-          json.dump(mt,fp)
-
-    res[k] = {'trans':mt[k],'meta':strs[k]}
-  if i>0:
-    with open(mtfile,'w') as fp:
-      json.dump(mt,fp)
+    if(k in mt):
+      res[k] = {'trans':mt[k],'meta':strs[k]}
+      continue
+    if(len(k)+len(txt)>=maxNumChars):
+      if maxNumChars==0:
+        txt = k
+      if translator.allowList:
+        txt = txt.split('\n')[:-1]
+      doBatchTranslate(txt,mt,res,strs)
+      txt=""
+    else:
+      txt+=k+'\n'
+    
+  if len(txt)>0:
+    doBatchTranslate(txt,mt,res,strs)
   return res
 
 
 def buildPO(mt,lang):
   po = polib.POFile(encoding='utf-8')
   po.metadata = {
-      'Project-Id-Version': '1.2.7',
+      'Project-Id-Version': '1.2.10',
       'Report-Msgid-Bugs-To': 'lab@organic-orchestra.com',
       'POT-Creation-Date': '2007-10-18 14:00+0100',
       'PO-Revision-Date': '2007-10-18 14:00+0100',
@@ -205,12 +303,13 @@ def buildPO(mt,lang):
     trs_s = v['trans'];
     relSrcFile = v['meta'][0][len(sourcePath)+1:]
     lineNum = v['meta'][1]
-    print (k,relSrcFile,lineNum)
+    # print (k,relSrcFile,lineNum)
     entry = polib.POEntry(
         msgid=k,
         msgstr=trs_s,
-        occurrences=[(relSrcFile,lineNum)]
+        
     )
+    entry.occurrences=[(relSrcFile,lineNum)]
     po.append(entry)
   po.save(os.path.join(baseTranslationPath,'%s.po'%lang['name']))
 
@@ -241,27 +340,21 @@ def getDefaultStrings():
 
 
 
-def buildRegFunction(fname,valid_mask,strict = True):
-  anys = r"[\s\n\r]*"
-  s = r'(?<!//)'+anys+fname+anys+"\("+anys
-  for m in  valid_mask:
-    if m:
-      s+=r'(?:String'+anys+'\(|)"(.+?)"'
-    else:
-      s+=r'.+'
-    s+=anys+','+anys
-  s = s[:-len(anys)-1]
-  if(strict) :
-    s+=anys+"\)"
-  print(s)
-  return re.compile(s,re.MULTILINE)
+def makeRawFile(tel):
+  with open(baseTranslationPath+ 'raw.txt','w') as fp:
+    fp.write('\n'.join(list(tel.keys())))
+
 
 
 if __name__ == "__main__":
 
-  # s = """NLOGE("Engine", String("File : 123 not found.").replace("123", fileArg));"""
-  # r = buildRegFunction("(?<!N)LOGE",[1,1],strict=False)
-  # print (re.findall(r,s))
+  # with open('/tmp/tstReg.txt' ,'r') as fp:
+  #   s = fp.read()
+  # r=buildRegFunction("ShapeShifterContentComponent",[0,1])
+  # print (s)
+  # it = re.finditer(r,s)
+  # for i in it:
+  #   print (i.groups())
   # exit()
 
   fl = getFileList()
@@ -271,6 +364,8 @@ if __name__ == "__main__":
 
   ds = getDefaultStrings()
   tel.update(ds)
+
+  makeRawFile(tel)
   # print (autoEl)
   langs  = [
   {'code':'fr','name':'french'},
