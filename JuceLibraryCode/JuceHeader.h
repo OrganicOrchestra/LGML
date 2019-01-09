0e31f1f0cfe8dfa4d1713bab3ca543a75a245bd3
Tintamar <martin.hermant@gmail.com>
2019-01-09 11:11:13 +0100

    bump to 1.3.1

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -43,7 +43,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "LGML";
     const char* const  companyName    = "Organic Orchestra";
-    const char* const  versionString  = "1.2.10";
-    const int          versionNumber  = 0x1020a;
+    const char* const  versionString  = "1.3.1";
+    const int          versionNumber  = 0x10301;
 }
 #endif
